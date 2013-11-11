6e680b14e79e862a503ef40c76f219988e72ca52
JordanJustice <jjustice@academic.rrc.ca>
2013-11-10 23:50:01 -0300

    Add PreferencesActivity

---
+++ ProjectTest/src/com/example/projecttest/PrefrencesActivity.java
@@ -0,0 +1,22 @@
+package com.example.projecttest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PrefrencesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_prefrences);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.prefrences, menu);
+		return true;
+	}
+
+}
