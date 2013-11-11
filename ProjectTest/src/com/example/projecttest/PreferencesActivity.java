34ee927d177afc86c8e561caca225101e1e8d5fa
JordanJustice <jjustice@academic.rrc.ca>
2013-11-11 00:16:08 -0300

    Fix sdk versions, add ZXing (zebra crossing) library

--- ProjectTest/src/com/example/projecttest/PreferencesActivity.java
+++ ProjectTest/src/com/example/projecttest/PreferencesActivity.java
@@ -9,13 +9,13 @@ public class PreferencesActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_prefrences);
+		setContentView(R.layout.activity_preferences);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.prefrences, menu);
+		getMenuInflater().inflate(R.menu.preferences, menu);
 		return true;
 	}
 
