340d2cc4f264bbf8b551b225dadae1ebdb2490d7
JordanJustice <jjustice@academic.rrc.ca>
2013-11-10 23:50:52 -0300

    Fix spelling mistake in PreferencesActivity

--- ProjectTest/src/com/example/projecttest/PreferencesActivity.java
+++ ProjectTest/src/com/example/projecttest/PreferencesActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class PrefrencesActivity extends Activity {
+public class PreferencesActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
