ee416f4539286f778af310747b5683a3b0ce7396
jjustice <jjustice@ACADEMIC.RRC.CA>
2013-11-12 01:35:59 -0300

    Add basic textviews and button for ZXing barcode scanning to OnClickLister activity_main.xml, add OnClickListener to scan button, receive scan activity intent with onActivityResult().

--- ProjectTest/src/com/example/projecttest/MainActivity.java
+++ ProjectTest/src/com/example/projecttest/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.projecttest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	TextView tvStatus, tvResult;
+	Button btnScan;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tvStatus = (TextView)findViewById(R.id.main_status_tv);
+		tvResult = (TextView)findViewById(R.id.main_result_tv);
+		btnScan = (Button)findViewById(R.id.main_scan_btn);
+		
+		btnScan.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				try {
+					Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+					intent.putExtra("SCAN_MODE", "ONE_D_MODE");
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					Toast.makeText(getApplicationContext(), "ERROR: " + ex, 1).show();
+				}
+			}
+		});		
 	}
 
 	@Override
@@ -19,4 +45,16 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == 0) {
+				tvStatus.setText(intent.getStringExtra("SCAN_RESULT_FORMAT"));
+				tvResult.setText(intent.getStringExtra("SCAN_RESULTS"));
+			} else if (resultCode == RESULT_CANCELED) {
+				tvStatus.setText(R.string.main_status_default);
+				tvResult.setText("Scan cancelled");
+			}
+		}
+	}
 }
